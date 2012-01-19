24119e4e2fcbf0b09ccfca38221a4db01a6de17a
a.slingsby <a.slingsby@soi.city.ac.uk>
2012-01-19 02:22:28 +0000

    Possible fix

--- Handy/src/org/gicentre/handy/HandyDraw.java
+++ Handy/src/org/gicentre/handy/HandyDraw.java
@@ -1,8 +1,12 @@
 package org.gicentre.handy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PGraphics2D;
+import processing.core.PVector;
 
 /**Acts as a Wrapper around HandyRenderer to make Handy drawing work like
  * other Processing drawing functions.
@@ -19,11 +23,16 @@ public class HandyDraw extends PGraphics2D{
 	PApplet sketch;
 	PGraphics sketchG;//keep track of the Sketch's original canvas
 	
+	boolean isDrawingShape=false;
+	List<PVector> vertices=new ArrayList<PVector>();
+	
+	boolean useSuper=false;
+	
 	public HandyDraw(PApplet sketch){
 		this.sketch=sketch;
 		this.sketchG=sketch.g;
 		this.handyRenderer=new HandyRenderer(sketch);
-		this.handyRenderer.setGraphics(this);
+		this.handyRenderer.setGraphics((PGraphics2D)this);
 		this.setSize(sketch.width, sketch.height);
 	}
 	
@@ -47,20 +56,111 @@ public class HandyDraw extends PGraphics2D{
 		this.sketch.noTint(); //temporarily remove tint
 		this.sketch.image(this.get(),0,0);
 		this.sketch.popStyle();
+		
+		//clear any incomplete shapes
+		isDrawingShape=false;
+		vertices.clear();
 	}
 	
 	public void line(float x1, float y1, float x2, float y2){
-		handyRenderer.line(x1, y1, x2, y2);
+		if (useSuper){
+			super.line(x1, y1, x2, y2);
+		}
+		else{
+			useSuper=true;
+			handyRenderer.line(x1, y1, x2, y2);
+			useSuper=false;
+		}
 	}
 	
-	//this method crashes
 	public void ellipse(float x, float y,float w, float h){
-		handyRenderer.ellipse(x, y, w, h);
+		if (useSuper){
+			super.ellipse(x,y,w,h);
+		}
+		else{
+			useSuper=true;
+			handyRenderer.ellipse(x, y, w, h);
+			useSuper=false;
+		}
 	}
 
-	//...but this one doesn't!
-//	public void rect(float x, float y,float w, float h){
-//		handyRenderer.ellipse(x, y, w, h);
+	public void rect(float x, float y,float w, float h){
+		if (useSuper){
+			super.rect(x,y,w,h);
+		}
+		else{
+			useSuper=true;
+			handyRenderer.rect(x, y, w, h);
+			useSuper=false;
+		}
+	}
+
+	public void triangle(float x1, float y1,float x2, float y2,float x3, float y3){
+		if (useSuper){
+			super.triangle(x1,y1,x2,y2,x3,y3);
+		}
+		else{
+			useSuper=true;
+			handyRenderer.triangle(x1,y1,x2,y2,x3,y3);
+			useSuper=false;
+		}
+	}
+
+	
+//	public void triangle(float x1, float y1,float x2, float y2,float x3, float y3){
+//		//a bit horrible, but will call the superclass' method if called by HandyRenderer
+//		//to prevent infinite recursion
+//		for(StackTraceElement stackTraceElement:Thread.currentThread().getStackTrace()){
+//			if (stackTraceElement.getClassName().equals(HandyRenderer.class.getName())){
+//				super.triangle(x1,y1,x2,y2,x3,y3);
+//				return;
+//			}
+//		}
+//		//otherwise, call HandyRenderer's method
+//		handyRenderer.triangle(x1,y1,x2,y2,x3,y3);
 //	}
+	
+	public void beginShape(){
+		if (useSuper){
+			super.beginShape();
+		}
+		else{
+			useSuper=true;
+			isDrawingShape=true;
+			useSuper=false;
+		}
+	}
+	
+	public void vertex(float x, float y){
+		if (useSuper){
+			super.vertex(x,y);
+		}
+		else{
+			useSuper=true;
+			vertices.add(new PVector(x,y));
+			useSuper=false;
+		}
+	}
+
+	public void endShape(){
+		if (useSuper){
+			super.endShape();
+		}
+		else{
+			useSuper=true;
+			float[] xs=new float[vertices.size()];
+			float[] ys=new float[vertices.size()];
+			int i=0;
+			for (PVector pVector:vertices){
+				xs[i]=pVector.x;
+				ys[i]=pVector.y;
+				i++;
+			}
+			handyRenderer.shape(xs,ys);
+			isDrawingShape=false;
+			vertices.clear();
+			useSuper=false;
+		}
+	}
 
 }
--- Handy/src/org/gicentre/tests/HandyDrawTest.java
+++ Handy/src/org/gicentre/tests/HandyDrawTest.java
@@ -11,23 +11,49 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class HandyDrawTest extends PApplet {
 
-	HandyDraw handyCanvas;
-	
+	HandyDraw handyDraw;
+	boolean useHandy=true;
+
 	public void setup(){
-		handyCanvas=new HandyDraw(this);
+		handyDraw=new HandyDraw(this);
 		smooth();
+		size(240,240);
 	}
-	
+
 	public void draw(){
 		background(255);
-		handyCanvas.startHandy();
+		fill(200,100,100); //styles are honoured by handyDraw even if set outside start/stopHandy
+
+		if (useHandy)
+			handyDraw.startHandy();
 		line(10, 10, 200, 200);
-		fill(200,100,100);
 		ellipse(100,100,20,20);
 		rect(150,150,30,30);
-		handyCanvas.stopHandy();
-		
+		triangle(40, 20, 20, 40, 60, 30);
+		beginShape();
+		vertex(180,30);
+		vertex(200,30);
+		vertex(200,10);
+		vertex(210,10);
+		vertex(210,30);
+		vertex(230,30);
+		vertex(230,40);
+		vertex(210,40);
+		vertex(210,60);
+		vertex(200,60);
+		vertex(200,40);
+		vertex(180,40);
+		endShape();
+		if (useHandy)
+			handyDraw.stopHandy();
+
 
 	}
-	
+
+	public void keyPressed(){
+		//toggle use handy
+		if (key=='h')
+			useHandy=!useHandy;
+	}
+
 }
